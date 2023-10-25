6c15c9dd9a1d15192ba681c4bd024cec1a4431c7
harshgupta0 <hg09032@gmail.com>
2023-10-26 00:18:44 +0530

    created staggeredgridView.countBuilder by reducing the version of package

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:keepnote/SideMenuBar.dart';
 import 'package:keepnote/constants.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -10,6 +11,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String note =
+      "THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE THIS IS NOTE";
+  String note1 = "THIS IS NOTE THIS IS NOTE THIS IS NOTE";
   final GlobalKey<ScaffoldState> _drawerKey = GlobalKey<ScaffoldState>();
 
   @override
@@ -20,119 +24,133 @@ class _HomeState extends State<Home> {
       drawer: SideMenu(),
       backgroundColor: mybgColor,
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(right: 10, left: 10),
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                width: MediaQuery.of(context).size.width,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: mycardColor,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: myblack,
-                      spreadRadius: 1 * 0.7,
-                      blurRadius: 2,
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        _drawerKey.currentState?.openDrawer();
-                      },
-                      icon: Icon(
-                        Icons.menu,
-                        color: mywhite,
-                        size: 30,
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: 10, left: 10),
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: mycardColor,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: myblack,
+                        spreadRadius: 1 * 0.7,
+                        blurRadius: 2,
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        "Search Your Note",
-                        style: TextStyle(fontSize: 18, color: Colors.grey),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.grid_view,
-                            color: mywhite,
-                            size: 28,
-                          ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _drawerKey.currentState?.openDrawer();
+                        },
+                        icon: Icon(
+                          Icons.menu,
+                          color: mywhite,
+                          size: 30,
                         ),
-                        SizedBox(width: 8),
-                        CircleAvatar(radius: 17),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      "ALL",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: mywhite.withOpacity(0.3),
                       ),
-                    ),
-                  ],
+                      Center(
+                        child: Text(
+                          "Search Your Note",
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.grid_view,
+                              color: mywhite,
+                              size: 28,
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          CircleAvatar(radius: 17),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(child: StaggeredGrid.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 4,
-                crossAxisSpacing: 4,
-                children:const [
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 2,
-                    child: Text("lollllll",style: TextStyle(color: Colors.white),),
+                Container(
+                  margin: EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "ALL",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: mywhite.withOpacity(0.3),
+                        ),
+                      ),
+                    ],
                   ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 1,
-                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Text("lollllll",style: TextStyle(color: Colors.white),),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 4,
-                    mainAxisCellCount: 2,
-                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
-                  ),
-                ],
-              ),
-              ),
-            ],
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 15,
+                    ),
+                    child: StaggeredGridView.countBuilder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 10,
+                        mainAxisSpacing: 12,
+                        crossAxisSpacing: 12,
+                        crossAxisCount: 4,
+                        staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+                        itemBuilder: (context, index) =>
+                            InkWell(
+                              onTap: ()
+                              {
+                                // Navigator.push(context, MaterialPageRoute(builder: (context) => NoteView()));
+                              },
+                              child:
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: mywhite.withOpacity(0.4)),
+                                    borderRadius: BorderRadius.circular(7)),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("HEADING",
+                                        style: TextStyle(
+                                            color: mywhite,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      index.isEven
+                                          ? note.length > 250
+                                          ? "${note.substring(0, 250)}..."
+                                          : note
+                                          : note1,
+                                      style: TextStyle(color: mywhite),
+                                    )
+                                  ],
+                                ),
+                              ),
+
+                            )
+                    )),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-
 }
-
-
-
-
