7121540228869631cda7f05702261e898ab70757
harshgupta0 <hg09032@gmail.com>
2023-10-25 22:30:32 +0530

    madesome changes

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:keepnote/SideMenuBar.dart';
 import 'package:keepnote/constants.dart';
-
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  GlobalKey<ScaffoldState> _drawerkey = GlobalKey();
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       endDrawerEnableOpenDragGesture: true,
-      key: _drawerkey,
+      key: _drawerKey,
       drawer: SideMenu(),
       backgroundColor: mybgColor,
       body: SafeArea(
@@ -23,8 +24,8 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               Container(
-                padding: EdgeInsets.only(right: 10,left: 10),
-                margin: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
+                padding: EdgeInsets.only(right: 10, left: 10),
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
                 width: MediaQuery.of(context).size.width,
                 height: 55,
                 decoration: BoxDecoration(
@@ -32,39 +33,106 @@ class _HomeState extends State<Home> {
                   borderRadius: BorderRadius.circular(8),
                   boxShadow: [
                     BoxShadow(
-                        color: myblack,
-                        spreadRadius: 1*.7,
-                        blurRadius: 2
+                      color: myblack,
+                      spreadRadius: 1 * 0.7,
+                      blurRadius: 2,
                     ),
                   ],
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(onPressed: (){
-                      _drawerkey.currentState?.openDrawer();
-                      }, icon: Icon(Icons.menu,color: mywhite,size: 30,)),
-                    Center(child: Text("Search Your Note",style: TextStyle(fontSize: 18,color: Colors.grey),),),
-                    Row(children: [
-                      IconButton(onPressed: (){}, icon: Icon(Icons.grid_view,color: mywhite,size: 28)),
-                      SizedBox(width: 8,),
-                      CircleAvatar(radius: 17,)
-                    ],)
+                    IconButton(
+                      onPressed: () {
+                        _drawerKey.currentState?.openDrawer();
+                      },
+                      icon: Icon(
+                        Icons.menu,
+                        color: mywhite,
+                        size: 30,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "Search Your Note",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.grid_view,
+                            color: mywhite,
+                            size: 28,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        CircleAvatar(radius: 17),
+                      ],
+                    ),
                   ],
                 ),
               ),
               Container(
                 margin: EdgeInsets.only(left: 20),
                 child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text("ALL",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: mywhite.withOpacity(.3)),),
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "ALL",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: mywhite.withOpacity(0.3),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(child: StaggeredGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                children:const [
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 2,
+                    child: Text("lollllll",style: TextStyle(color: Colors.white),),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 1,
+                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Text("lollllll",style: TextStyle(color: Colors.white),),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 4,
+                    mainAxisCellCount: 2,
+                    child:Text("lollllll",style: TextStyle(color: Colors.white),),
+                  ),
                 ],
-              ),)
+              ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
 }
+
+
+
+
