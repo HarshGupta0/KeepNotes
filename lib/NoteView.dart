c790bc77d6e32bd488c764d94e5d07de18e1ad23
harshgupta0 <hg09032@gmail.com>
2023-10-27 02:43:47 +0530

    made some changes

--- lib/NoteView.dart
+++ lib/NoteView.dart
@@ -17,24 +17,7 @@ class _NoteViewState extends State<NoteView> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: mybgColor,
-        actions: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                  children: [BackButton(color: Colors.white,),],
-                ),
-                  Row(
-                    children: [
-                      IconButton(onPressed:(){}, icon:Icon(CupertinoIcons.pin,color: mywhite,),),
-                      IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell_solid,color: mywhite,),),
-                      IconButton(onPressed: (){}, icon: Icon(Icons.archive,color: mywhite,),),
-                    ],
-                  ),
-                ],
-              )
-        ],
+
       ),
     ));
   }
